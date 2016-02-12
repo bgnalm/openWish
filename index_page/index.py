2807b7a6a906e0af80530681e2429273eb8daa33
Michael Lellouch <michael.lelleouch@gmail.com>
2016-02-13 01:43:20 +0200

    added index exception stuff

--- index_page/index.py
+++ index_page/index.py
@@ -3,9 +3,9 @@ HTML_INDEX_PAGE = 'index.html'
 def main():
 	try:
 		html_file = open(HTML_INDEX_PAGE, 'r')
-	except:
-		return 'error: open index.html'
-		
+	except Exception ,e:
+		return 'error: open index.html\n' + str(e) 
+
 	html_text = html_file.read()
 	html_file.close()
 	return html_text
