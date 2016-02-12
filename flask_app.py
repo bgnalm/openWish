2e574ce0450d997647e029076cb1a9555747e643
Michael Lellouch <michael.lelleouch@gmail.com>
2016-02-13 01:57:29 +0200

    master: test

--- flask_app.py
+++ flask_app.py
@@ -12,10 +12,13 @@ app = Flask(__name__)
 @app.route("/", methods=['POST', 'GET'])
 @app.route("/index", methods=['POST', 'GET'])
 def route_index():
+    """
     os.chdir('./index_page')
     returned_data = index_page.main()
     os.chdir('..')
     return returned_data    
+    """
+    return os.listdir('.')
 
 def main():
     print 'started at {0}'.format(time.asctime())
