3c2bfc36bcc271050fa4153587c58d6574df5650
Michael Lellouch <michael.lelleouch@gmail.com>
2016-02-13 01:46:03 +0200

    master: handling different dirs

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 import time
+import os
 
 import index_page.index as index_page
 
@@ -10,8 +11,11 @@ app = Flask(__name__)
 @app.route("/test", methods=['POST', 'GET'])
 @app.route("/", methods=['POST', 'GET'])
 @app.route("/index", methods=['POST', 'GET'])
-def route_test():
-    return index_page.main()
+def route_index():
+	os.chdir('index_page')
+    returned_data = index_page.main()
+    os.chdir('..')
+    return returned_data
 
 def main():
 	print 'started at {0}'.format(time.asctime())
