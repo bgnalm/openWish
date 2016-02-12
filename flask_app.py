2733fac782d11ad86e061921e49a2b68c126d2b8
Michael Lellouch <michael.lelleouch@gmail.com>
2016-02-13 01:51:33 +0200

    tabs --> 4 spaces

--- flask_app.py
+++ flask_app.py
@@ -12,10 +12,10 @@ app = Flask(__name__)
 @app.route("/", methods=['POST', 'GET'])
 @app.route("/index", methods=['POST', 'GET'])
 def route_index():
-	os.chdir('index_page')
+    os.chdir('index_page')
     returned_data = index_page.main()
     os.chdir('..')
-    return returned_data
+    return returned_data    
 
 def main():
-	print 'started at {0}'.format(time.asctime())
+    print 'started at {0}'.format(time.asctime())
