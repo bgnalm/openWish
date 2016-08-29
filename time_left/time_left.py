65949c83bbf8262041d294adfe9077ecbd689737
Michael Lellouch <michael.lelleouch@gmail.com>
2016-08-29 03:03:42 +0300

    updated time_left. todo: update add wish so its updating the next read field

--- time_left/time_left.py
+++ time_left/time_left.py
@@ -1,12 +1,31 @@
 import time
 
-REQUIRED_FIELDS = ['name']
+REQUIRED_FIELDS = ['user_id']
+
+def can_user_post(db, user_id):
+	next_read = db.next_read(request['user_id'])
+	if next_read < int(time.time()):
+		return True
+
+	return False
+
 
 def main(db, request, consts):
-	next_read = db.next_read(request['name'])
-	seconds_left = next_read - int(time.time())
-	if seconds_left < 0:
-		seconds_left = 0
+	try:
+		next_read = db.next_read(request['user_id'])
+		seconds_left = next_read - int(time.time())
 
-	return str(seconds_left)
+		return {
+			'success' : True,
+			'data' : {
+				'can_post' : (seconds_left > 0),
+				'when_will_post' : time.ctime(next_read)
+			}
+		}
+
+	except Exception, e:
+		return {
+			'success' : False,
+			'message' : e.message
+		}
 
