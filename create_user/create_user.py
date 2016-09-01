ed6a46e51da01f6d97b87a6f9b74430a556d5420
Michael Lellouch <michael.lelleouch@gmail.com>
2016-09-01 13:17:50 +0300

    before leaving2, todo: finish_read_wish, and test all recent changes

---
+++ create_user/create_user.py
@@ -0,0 +1,19 @@
+import time
+
+REQUIRED_FIELDS = ['user_name']
+
+def main(db, request, consts):
+
+	try:
+		return {
+			'success' : True,
+			'data' : {
+				'user_id' : str(db.create_user(request['user_name']))
+			}
+		}
+
+	except Exception, e:
+		return {
+			'success' : False,
+			'message' : e.message
+		}
---
+++ read_wish/read_wish.py
@@ -0,0 +1,23 @@
+REQUIRED_FIELDS = ['user_name']
+
+def main(db, requests, consts):
+	try:
+		wish = db.get_random_wish(reader_user_name=request['user_name'], add_to_read_wishes=True)
+		return {
+			'success' : True,
+			'data' : {
+				'text' : wish.text,
+				'time_added' : wish.time_added,
+				'user_name' : wish.user_name,
+				'rating' : wish.rating,
+				'number_of_reads' : wish.number_of_reads
+				'optinal' : wish.optinal
+			}
+		}
+
+	except Exception, e:
+		return {
+			'success' : False,
+			'message' : e.message
+		}
+		
